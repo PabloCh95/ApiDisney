1eaa28de3049f24ea64c2e60778f60e359d164f6
PabloCh95 <pablo.ch98@gmail.com>
2021-04-30 19:42:37 -0300

    JSON WEB TOKEN IMPLEMENTADO

--- src/services/jwt.js
+++ src/services/jwt.js
@@ -1,11 +1,36 @@
 const jwt=require('jwt-simple');
-const {DateTime,Duration}=require('luxon');
+const moment= require('moment');//se que moment no va mas, pero bueno mas adelante lo modifico con luxon
 const SECRET_KEY=process.env.SECRET_KEY || "ASÑDJQPWJEI123AKSPDJ123";
 
-const createToken=()=>{
-    console.log(DateTime.now().ts);
-    
-    console.log(Duration.hours)
+const createToken=(user)=>{
+    const payload ={
+        id:user._id,
+        name:user.name,
+        lastname:user.lastname,
+        email:user.email,
+        role:user.role,
+        createToken: moment().unix(),
+        exp:moment().add(3,"hours").unix()
+    };
+
+    return jwt.encode(payload,SECRET_KEY);
 }
 
-module.exports=createToken
+const createRefreshToken = (user)=>{
+    const payload ={
+        id:user._id,
+        exp:moment().add(30,'days').unix()
+    };
+
+    return jwt.encode(payload,SECRET_KEY);
+};
+
+const decodedToken = (token)=>{
+    return jwt.decode(token,SECRET_KEY,true);
+}
+
+module.exports={
+    createToken,
+    decodedToken,
+    createRefreshToken
+}
